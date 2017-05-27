080ba18ed745025500a1813767ada5d9cab58c3a
Farhad2015 <mrforhad1@gmail.com>
2017-05-27 17:44:54 +0600

    changes: added animation.

--- app/src/main/java/ezzetech/com/newsdemo/adapters/NewsAdapter.java
+++ app/src/main/java/ezzetech/com/newsdemo/adapters/NewsAdapter.java
@@ -5,6 +5,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,6 +16,7 @@ import com.bumptech.glide.Glide;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
+import java.util.Random;
 
 import ezzetech.com.newsdemo.R;
 import ezzetech.com.newsdemo.model.NewsItem;
@@ -21,6 +26,8 @@ import ezzetech.com.newsdemo.model.NewsItem;
  */
 
 public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsViewHolder> {
+
+    private int lastPosition = -1;
 
     List<NewsItem> nItem;
     Context context;
@@ -64,10 +71,20 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsViewHolder
         holder.textViewTitle.setText(newsItem.getNewsTitle());
         holder.textViewDescription.setText(newsItem.getNewsDetails());
         holder.textViewNewsSource.setText("more at " + newsItem.getProviderName());
+
+        setAnimation(holder.itemView, position);
     }
 
     @Override
     public int getItemCount() {
         return nItem.size();
+    }
+
+    private void setAnimation(View viewToAnimate, int position) {
+        if (position > lastPosition) {
+            Animation animation = AnimationUtils.loadAnimation(context, android.R.anim.slide_in_left);
+            viewToAnimate.startAnimation(animation);
+            lastPosition = position;
+        }
     }
 }
